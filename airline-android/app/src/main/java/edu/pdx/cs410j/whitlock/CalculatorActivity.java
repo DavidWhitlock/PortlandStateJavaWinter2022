2b5d9b22955dcfe1b2bfa05d051f490ff4b4ca65
David Whitlock <david.m.whitlock@gmail.com>
2022-03-02 19:19:01 -0800

    Basic logic for calculator.

--- airline-android/app/src/main/java/edu/pdx/cs410j/whitlock/CalculatorActivity.java
+++ airline-android/app/src/main/java/edu/pdx/cs410j/whitlock/CalculatorActivity.java
@@ -3,6 +3,9 @@ package edu.pdx.cs410j.whitlock;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class CalculatorActivity extends AppCompatActivity {
 
@@ -10,5 +13,19 @@ public class CalculatorActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calculator);
+    }
+
+
+    public void calculateSum(View view) {
+        EditText leftOperand = findViewById(R.id.leftOperand);
+        EditText rightOperand = findViewById(R.id.rightOperand);
+
+        String left = leftOperand.getText().toString();
+        String right = rightOperand.getText().toString();
+
+        int sum = Integer.parseInt(left) + Integer.parseInt(right);
+
+        TextView sumField = findViewById(R.id.sum);
+        sumField.setText(String.valueOf(sum));
     }
 }
