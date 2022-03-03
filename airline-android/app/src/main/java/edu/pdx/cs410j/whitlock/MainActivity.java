080239390c1edd2b667a34563ad31f12e1ad85ee
David Whitlock <david.m.whitlock@gmail.com>
2022-03-02 18:36:48 -0800

    Launch the Calculator activity using an Intent.

--- airline-android/app/src/main/java/edu/pdx/cs410j/whitlock/MainActivity.java
+++ airline-android/app/src/main/java/edu/pdx/cs410j/whitlock/MainActivity.java
@@ -2,6 +2,7 @@ package edu.pdx.cs410j.whitlock;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -15,8 +16,9 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-    public void displayButtonClicked(View view) {
-        Toast.makeText(MainActivity.this, "You clicked a button again!", Toast.LENGTH_LONG).show();
+    public void launchCalculator(View view) {
+        Intent intent = new Intent(MainActivity.this, CalculatorActivity.class);
+        startActivity(intent);
     }
 
 }
