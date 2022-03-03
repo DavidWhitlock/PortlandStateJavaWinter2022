085bd82088dd2eeb2d81d46cdac9b1ac85b9b7dc
David Whitlock <david.m.whitlock@gmail.com>
2022-03-02 18:21:54 -0800

    Added a button listener

--- airline-android/app/src/main/java/edu/pdx/cs410j/whitlock/MainActivity.java
+++ airline-android/app/src/main/java/edu/pdx/cs410j/whitlock/MainActivity.java
@@ -3,6 +3,9 @@ package edu.pdx.cs410j.whitlock;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button button = findViewById(R.id.button2);
+        button.setOnClickListener(view -> Toast.makeText(MainActivity.this, "You clicked a button!", Toast.LENGTH_LONG).show());
     }
 }
