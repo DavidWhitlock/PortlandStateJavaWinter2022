6f801b56b67f351686e2aaae84a2d59eba6af86b
David Whitlock <david.m.whitlock@gmail.com>
2022-03-09 18:20:52 -0800

    Added a ListView widget that is populated with ints.

--- airline-android/app/src/main/java/edu/pdx/cs410j/whitlock/MainActivity.java
+++ airline-android/app/src/main/java/edu/pdx/cs410j/whitlock/MainActivity.java
@@ -5,19 +5,32 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ArrayAdapter<Integer> sums;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        this.sums = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1);
+
+        ListView listView = findViewById(R.id.sums);
+        listView.setAdapter(this.sums);
+
+        for (int i = 0; i < 50; i++) {
+            this.sums.add(i);
+        }
     }
 
     public void launchCalculator(View view) {
-        Intent intent = new Intent(MainActivity.this, CalculatorActivity.class);
+        Intent intent = new Intent(this, CalculatorActivity.class);
         startActivity(intent);
     }
 
